881bf10e81fdafb2bbd34e00b51a90edf52db143
Svahn <svahco@corp.delaval.com>
2019-04-25 17:01:20 +0200

    Removed Azure stuff

--- sio-server/src/app.js
+++ sio-server/src/app.js
@@ -1,13 +1,13 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var documents = {};
+const documents = {};
 
 io.on("connection", socket => {
     console.log('connection');
-    var previousId;
-    var safeJoin = currentId => {
+    let previousId;
+    const safeJoin = currentId => {
         console.log('connection');
       socket.leave(previousId);
       socket.join(currentId);
@@ -38,5 +38,5 @@ io.on("connection", socket => {
     io.emit("documents", Object.keys(documents));
   });
 
-  console.log('started @' + process.env.port);
-  http.listen(process.env.port);
+  console.log('started @4444');
+  http.listen(4444);
